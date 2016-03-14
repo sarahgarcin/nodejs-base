0b568d8bfa813aa7427a3b23745614014f3f8bc5
sarahgarcin <sarahgarcin@hotmail.com>
2016-03-14 19:10:06 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var fs = require('fs-extra'),
+	glob = require("glob"),
+	path = require("path");
+
+module.exports = function(app, io){
+
+	console.log("main module initialized");
+
+	io.on("connection", function(socket){
+
+	});
+
+
+// ------------- F U N C T I O N S -------------------
+
+
+
+// - - - END FUNCTIONS - - - 
+};
